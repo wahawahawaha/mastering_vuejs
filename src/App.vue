908e22c9055778d05b5794d9e5e1dbb49566d1c2
wahawahawaha <chengdong93546@gmail.com>
2025-12-29 11:58:41 +0900

    myFirstCommit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, reactive, watchEffect } from 'vue'
+
+const user = reactive({
+  name: '',
+  age: '',
+})
+</script>
+
+<template>
+  <h2>{{ user.name }}さんは{{ user.age }}歳</h2>
+  <input v-model="user.name" placeholder="名前" />
+  <input v-model="user.age" placeholder="年齢" />
+</template>
+
+<style></style>
